ee5bde8f1fe60a40ca386fe2988d069a5cf99efa
Cookiezby <zhubingyi1992@gmail.com>
2019-12-08 19:05:06 +0900

    2019-12-08 19:05:03

--- post.sh
+++ post.sh
@@ -2,7 +2,7 @@
 CURRENTDATETIME=`date +"%Y-%m-%d %T"`
 bundle exec jekyll build
 cd images
-for i in *.jpg; do echo "Processing $i"; exiftool -all= "$i"; done
+for i in *.jpg; do echo "Processing $i"; exiftool -all= "$i"; rm "$i"_original;  done
 cd ../
 git add --all
 git commit -m "$CURRENTDATETIME"
